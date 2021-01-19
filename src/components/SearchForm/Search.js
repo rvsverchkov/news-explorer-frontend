c8eec46d1b04c4e7dc97d34c47adbdf0edb31bf4
rvsverchkov <s10z40@yandex.ru>
2021-01-19 22:04:34 +0300

    Diploma Project - Adaptive 320

--- src/components/SearchForm/Search.js
+++ src/components/SearchForm/Search.js
@@ -6,8 +6,12 @@ function Search (props) {
             <h1 className="search__title">Что творится в мире?</h1>
             <p className="search__subtitle">Находите самые свежие статьи на любую тему и сохраняйте в своём личном кабинете</p>
             <form className="search__form">
-                <input className="search__input" placeholder="Введите тему новости"/>
-                <button className="search__button" onClick={props.handleSearchClick}>Искать</button>
+                <div className="search__input-container">
+                    <input className="search__input" placeholder="Введите тему новости"/>
+                </div>
+                <div className="search__button-container">
+                    <button className="search__button" onClick={props.handleSearchClick}>Искать</button>
+                </div>
             </form>
         </div>
     )
